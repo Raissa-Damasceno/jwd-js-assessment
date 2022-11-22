8c460b25f8c52d96b857244a7a249c115384183c
Raissa <raissa.cod@gmail.com>
2022-11-22 13:58:56 +1100

    assessment completed

--- js/index.js
+++ js/index.js
@@ -19,37 +19,54 @@
       5. Add a countdown timer - when the time is up, end the quiz, display the score and highlight the correct answers
 *************************** */
 
-window.addEventListener('DOMContentLoaded', () => {
-  const start = document.querySelector('#start');
-  start.addEventListener('click', function (e) {
-    document.querySelector('#quizBlock').style.display = 'block';
-    start.style.display = 'none';
+window.addEventListener("DOMContentLoaded", () => {
+  const start = document.querySelector("#start");
+  start.addEventListener("click", function (e) {
+    document.querySelector("#quizBlock").style.display = "block";
+    start.style.display = "none";
+    document.querySelector("#btnSubmit").disabled = false;
+    document.getElementById("time").innerHTML = 1 + ":" + 00;
+    startTimer();
   });
   // quizArray QUESTIONS & ANSWERS
   // q = QUESTION, o = OPTIONS, a = CORRECT ANSWER
   // Basic ideas from https://code-boxx.com/simple-javascript-quiz/
   const quizArray = [
     {
-      q: 'Which is the third planet from the sun?',
-      o: ['Saturn', 'Earth', 'Pluto', 'Mars'],
-      a: 1, // array index 1 - so Earth is the correct answer here
+      q: "Which is the third planet from the sun?",
+      o: ["Saturn", "Earth", "Pluto", "Mars"],
+      a: 1,
     },
     {
-      q: 'Which is the largest ocean on Earth?',
-      o: ['Atlantic Ocean', 'Indian Ocean', 'Arctic Ocean', 'Pacific Ocean'],
+      q: "Which is the largest ocean on Earth?",
+      o: ["Atlantic Ocean", "Indian Ocean", "Arctic Ocean", "Pacific Ocean"],
       a: 3,
     },
     {
-      q: 'What is the capital of Australia',
-      o: ['Sydney', 'Canberra', 'Melbourne', 'Perth'],
+      q: "What is the capital of Australia",
+      o: ["Sydney", "Canberra", "Melbourne", "Perth"],
+      a: 1,
+    },
+    {
+      q: "The centre of a solar system is:",
+      o: ["star", "planet", "moon", "you"],
+      a: 0,
+    },
+    {
+      q: "The estimated age of the universe is:",
+      o: [
+        "about 50 million years old",
+        "between 7 and 20 million years old",
+        "between 1 and 5 billion years old",
+        "less than 1 billion years old",
+      ],
       a: 1,
     },
   ];
 
-  // function to Display the quiz questions and answers from the object
   const displayQuiz = () => {
-    const quizWrap = document.querySelector('#quizWrap');
-    let quizDisplay = '';
+    const quizWrap = document.querySelector("#quizWrap");
+    let quizDisplay = "";
     quizArray.map((quizItem, index) => {
       quizDisplay += `<ul class="list-group">
                    Q - ${quizItem.q}
@@ -63,28 +80,73 @@ window.addEventListener('DOMContentLoaded', () => {
     });
   };
 
-  // Calculate the score
   const calculateScore = () => {
     let score = 0;
+
     quizArray.map((quizItem, index) => {
       for (let i = 0; i < 4; i++) {
-        //highlight the li if it is the correct answer
         let li = `li_${index}_${i}`;
         let r = `radio_${index}_${i}`;
-        liElement = document.querySelector('#' + li);
-        radioElement = document.querySelector('#' + r);
+        liElement = document.querySelector("#" + li);
+        radioElement = document.querySelector("#" + r);
 
         if (quizItem.a == i) {
-          //change background color of li element here
-        }
-
-        if (radioElement.checked) {
-          // code for task 1 goes here
+          liElement.style.backgroundColor = "hsla(179, 94%, 21%, 0.5)";
+          if (radioElement.checked) {
+            score++;
+          }
         }
       }
     });
+
+    let scoreSpan = document.querySelector("#score");
+    scoreSpan.innerHTML = `Score: ${score}/${quizArray.length}`;
+    document.querySelector("#btnSubmit").disabled = true;
+    clearTimeout(timerId);
   };
 
-  // call the displayQuiz function
+  let btnSubmit = document.querySelector("#btnSubmit");
+  btnSubmit.addEventListener("click", calculateScore);
+
+  function startTimer() {
+    let nowTime = document.getElementById("time").innerHTML;
+    let timeArray = nowTime.split(/[:]+/);
+    console.log(timeArray);
+    let min = timeArray[0];
+    let sec = timeArray[1] - 1;
+    if (sec < 0) {
+      sec = 59;
+      min -= 1;
+    } else if (sec < 10) {
+      sec = "0" + sec;
+    }
+
+    document.getElementById("time").innerHTML = min + ":" + sec;
+
+    if (min === "0" && sec === "00") {
+      document.getElementById("time").style.color = "red";
+      document.getElementById("time").innerHTML += ` Timer Expired!!`;
+      calculateScore();
+      return;
+    }
+
+    timerId = setTimeout(startTimer, 1000);
+  }
+
   displayQuiz();
+
+  document.getElementById("btnReset").addEventListener("click", () => {
+    let liArray = document.getElementsByClassName("list-group-item");
+
+    for (let li of liArray) {
+      li.style.backgroundColor = "";
+      li.childNodes[0].checked = false;
+    }
+    clearTimeout(timerId);
+    document.getElementById("time").style.color = "";
+    document.querySelector("#score").innerHTML = "";
+    document.querySelector("#btnSubmit").disabled = false;
+    document.querySelector("#quizBlock").style.display = "none";
+    start.style.display = "block";
+  });
 });
